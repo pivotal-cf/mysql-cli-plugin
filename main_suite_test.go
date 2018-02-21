2cc4b0f46da1f660d820150bd76161e6e02a93df
Andrew Garner <agarner@pivotal.io>
2018-02-21 11:30:51 -0600

    Add end-to-end test for running the plugin
    - whitespace issues with checking for cf service creation / deletion
      seems to be a lingering issue
    
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

---
+++ main_suite_test.go
@@ -0,0 +1,32 @@
+package main_test
+
+import (
+	"os/exec"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+)
+
+func TestMysqlV2CliPlugin(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "MysqlV2CliPlugin Suite")
+}
+
+var _ = BeforeSuite(func() {
+	binaryPath, err := gexec.Build("github.com/pivotal-cf/mysql-v2-cli-plugin")
+	Expect(err).NotTo(HaveOccurred())
+
+	command := exec.Command("cf", "install-plugin", binaryPath, "-f")
+	session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session).Should(gexec.Exit(0))
+})
+
+var _ = AfterSuite(func() {
+	command := exec.Command("cf", "uninstall-plugin", "MysqlMigrate")
+	session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session).Should(gexec.Exit())
+})
---
+++ main_test.go
@@ -0,0 +1,54 @@
+package main_test
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
+	"github.com/pivotal-cf/mysql-v2-cli-plugin/test_helpers"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("MysqlV2CliPlugin", func() {
+
+	var (
+		donorService   string
+		restoreService string
+	)
+
+	BeforeEach(func() {
+		donorService = generator.PrefixedRandomName("MYSQL", "DED")
+		test_helpers.CreateService(os.Getenv("SERVICE_NAME"), os.Getenv("PLAN_NAME"), donorService)
+
+		restoreService = generator.PrefixedRandomName("MYSQL", "DED")
+		test_helpers.CreateService(os.Getenv("SERVICE_NAME"), os.Getenv("PLAN_NAME"), restoreService)
+
+		test_helpers.WaitForService(donorService, "status:    create succeeded")
+		test_helpers.WaitForService(restoreService, "status:    create succeeded")
+	})
+
+	AfterEach(func() {
+		test_helpers.DeleteService(donorService)
+		test_helpers.DeleteService(restoreService)
+		test_helpers.WaitForService(donorService, fmt.Sprintf("Service instance %s not found", donorService))
+		test_helpers.WaitForService(restoreService, fmt.Sprintf("Service instance %s not found", restoreService))
+	})
+
+	It("migrates the contents of one database to another", func() {
+		By("writing some information to the donor instance")
+		donorDeploymentName := test_helpers.GetDeploymentName(donorService)
+		writeStmt := `CREATE DATABASE pickles; CREATE TABLE pickles.ketchup(num INT PRIMARY KEY); INSERT INTO pickles.ketchup values(1),(2),(3);`
+		test_helpers.ExecuteMysqlQueryAsAdmin(donorDeploymentName, "0", writeStmt)
+
+		By("running the plugin")
+		test_helpers.ExecuteCfCmd("mysql-migrate", donorService, restoreService)
+
+		By("seeing the data on the restore service")
+		dataOnFollower := test_helpers.ExecuteMysqlQueryAsAdmin(restoreService, "1", "SELECT num FROM pickles.ketchup")
+		Expect(dataOnFollower).To(ContainSubstring("1"))
+		Expect(dataOnFollower).To(ContainSubstring("2"))
+		Expect(dataOnFollower).To(ContainSubstring("3"))
+	})
+})
