51af80af4dbd67f05dbdb5cc537a059d26ad8a01
Andrew Garner <agarner@pivotal.io>
2018-03-09 15:55:36 -0500

    Remove unused main_suite_test imports
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- main_suite_test.go
+++ main_suite_test.go
@@ -1,12 +1,10 @@
 package main_test
 
 import (
-	"os/exec"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gexec"
 )
 
 func TestMysqlV2CliPlugin(t *testing.T) {
