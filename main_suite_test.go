35209b8fedc02819abf4381e75d58a7f1fab8f96
Anthony Emengo <aemengo@pivotal.io>
2018-03-23 17:20:11 -0500

    Build plugin in main test suite for ui tests
    [#156140076]
    
    Signed-off-by: Andrew Garner <agarner@pivotal.io>

--- main_suite_test.go
+++ main_suite_test.go
@@ -1,13 +1,32 @@
 package main_test
 
 import (
+	"os/exec"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 )
 
 func TestMysqlCLIPlugin(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "TestMysqlCLIPlugin Suite")
 }
+
+var _ = BeforeSuite(func() {
+	cmd := exec.Command("go", "build", "-ldflags=-X 'main.version=0.0.1'", ".")
+	Expect(cmd.Run()).To(Succeed())
+
+	cmd = exec.Command("cf", "install-plugin", "-f", "mysql-cli-plugin")
+	session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session, "60s").Should(gexec.Exit(0))
+})
+
+var _ = AfterSuite(func() {
+	cmd := exec.Command("cf", "uninstall-plugin", "MysqlTools")
+	session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session, "60s").Should(gexec.Exit())
+})
