0974d1e30806faf0bd86c372dc6566e542b94fa1
Andy Gilbert <agilbert@pivotal.io>
2018-03-29 11:56:20 -0700

    Use cf test helpers so we can run ci instead of PWS.
    [#155919412]
    
    Signed-off-by: Anthony Emengo <aemengo@pivotal.io>

--- specs/specs_suite_test.go
+++ specs/specs_suite_test.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/cloudfoundry-incubator/cf-test-helpers/config"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/workflowhelpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -13,6 +15,22 @@ func TestIntegration(t *testing.T) {
 	RunSpecs(t, "Acceptance Tests Suite")
 }
 
+var (
+	TestSetup *workflowhelpers.ReproducibleTestSuiteSetup
+	Config    *config.Config
+)
+
 var _ = BeforeSuite(func() {
+	Config = config.LoadConfig()
+
+	TestSetup = workflowhelpers.NewTestSuiteSetup(Config)
+	TestSetup.Setup()
+
 	Expect(os.Setenv("CF_COLOR", "false")).To(Succeed())
+})
+
+var _ = AfterSuite(func() {
+	if TestSetup != nil {
+		TestSetup.Teardown()
+	}
 })
