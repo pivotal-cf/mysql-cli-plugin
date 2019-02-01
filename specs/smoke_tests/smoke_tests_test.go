34c3eb603e0af94be9fba3cbc3f923e07db9b0fa
Andrew Garner <agarner@pivotal.io>
2019-01-31 21:59:41 -0600

    Bump smoke tests timeout to 45 minutes
    Creating 4 node PXC clusters takes a long time.
    
    [#163427128]

--- specs/smoke_tests/smoke_tests_test.go
+++ specs/smoke_tests/smoke_tests_test.go
@@ -1,3 +1,15 @@
+// Copyright (C) 2019-Present Pivotal Software, Inc. All rights reserved.
+//
+// This program and the accompanying materials are made available under the terms of the under the Apache License,
+// Version 2.0 (the "License”); you may not use this file except in compliance with the License. You may obtain a copy
+// of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 package smoke_tests_test
 
 import (
@@ -86,7 +98,7 @@ var _ = Describe("SmokeTests", func() {
 				cmd := exec.Command("cf", "mysql-tools", "migrate", instanceName, destPlan)
 				session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
 				Expect(err).NotTo(HaveOccurred())
-				Eventually(session, "20m", "1s").Should(gexec.Exit(0))
+				Eventually(session, "45m", "1s").Should(gexec.Exit(0))
 			})
 
 			By("Verifying the destination service was renamed to the source's name", func() {
