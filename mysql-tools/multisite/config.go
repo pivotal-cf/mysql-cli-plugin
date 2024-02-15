b7ed59c3d807bc15d3723cd38ce035d87e75c332
Andrew Garner <garnera@vmware.com>
2024-02-15 11:16:20 -0600

    introduce multi-site config abstraction
    [#186582809](https://www.pivotaltracker.com/story/show/186582809)

---
+++ mysql-tools/multisite/config.go
@@ -0,0 +1,172 @@
+package multisite
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"code.cloudfoundry.org/cli/cf/configuration/confighelpers"
+)
+
+// TODO: There are various testing gaps at the corner cases where interacting with the filesystem could fail in strange ways
+//       We could test these corner by plumbing in a filesystem abstraction of some sort.
+//       It is hard to make those corner cases fail on a "real" system, otherwise.
+
+type Config struct {
+	Dir string
+}
+
+type Target struct {
+	Name         string
+	Organization string
+	Space        string
+	API          string
+}
+
+func (t *Target) Validate() error {
+	var missingFields []string
+
+	if t.API == "" {
+		missingFields = append(missingFields, "API endpoint")
+	}
+	if t.Organization == "" {
+		missingFields = append(missingFields, "Organization")
+	}
+	if t.Space == "" {
+		missingFields = append(missingFields, "Space")
+	}
+
+	if len(missingFields) != 0 {
+		return fmt.Errorf("missing fields: [%s]", strings.Join(missingFields, ","))
+	}
+
+	return nil
+}
+
+func (t *Target) UnmarshalJSON(b []byte) error {
+	var data struct {
+		OrganizationFields struct {
+			Name string
+		} `json:"OrganizationFields"`
+		SpaceFields struct {
+			Name string
+		} `json:"SpaceFields"`
+		Target string `json:"Target"`
+	}
+
+	if err := json.Unmarshal(b, &data); err != nil {
+		return err
+	}
+
+	t.API = data.Target
+	t.Organization = data.OrganizationFields.Name
+	t.Space = data.SpaceFields.Name
+
+	return nil
+}
+
+func NewConfig() (Config, error) {
+	cfCliConfig, err := confighelpers.DefaultFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	dir := filepath.Join(filepath.Dir(cfCliConfig), ".mysql-tools")
+
+	return Config{Dir: dir}, nil
+}
+
+func (c Config) ListConfigs() (targets []Target, errs error) {
+	files, err := filepath.Glob(filepath.Join(c.Dir, "*", ".cf", "config.json"))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, configFilePath := range files {
+		configName := filepath.Base(filepath.Dir(filepath.Dir(configFilePath)))
+		t := Target{Name: configName}
+		if err = c.parseTargetFromPath(configFilePath, &t); err != nil {
+			errs = errors.Join(errs, err)
+			return nil, err
+		}
+		targets = append(targets, t)
+	}
+
+	return targets, errs
+}
+
+func (Config) parseTargetFromPath(path string, t *Target) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = f.Close() }()
+	return json.NewDecoder(f).Decode(t)
+}
+
+func (c Config) SaveConfig(path, name string) (Target, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return Target{}, err
+	}
+	defer func() { _ = f.Close() }()
+
+	t := Target{Name: name}
+	if err := json.NewDecoder(f).Decode(&t); err != nil {
+		return Target{}, err
+	}
+
+	if err := t.Validate(); err != nil {
+		return Target{}, fmt.Errorf("saved configuration must target Cloudfoundry: %s", err)
+	}
+
+	targetConfig := filepath.Join(c.Dir, name, ".cf", "config.json")
+
+	// what permissions should we put on the dir?
+	// should we be testing this?
+	if err := os.MkdirAll(filepath.Dir(targetConfig), 0700); err != nil {
+		return Target{}, err
+	}
+
+	if _, err := f.Seek(0, io.SeekStart); err != nil {
+		return Target{}, fmt.Errorf("failed to reset offset in source file %s: %s", f.Name(), err)
+	}
+
+	w, err := os.OpenFile(targetConfig, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
+	if err != nil {
+		return Target{}, err
+	}
+	defer func() { _ = w.Close() }()
+
+	if _, err := io.Copy(w, f); err != nil {
+		return Target{}, err
+	}
+
+	if err := w.Close(); err != nil {
+		return Target{}, err
+	}
+
+	return t, nil
+}
+
+func (c Config) RemoveConfig(name string) error {
+	path := filepath.Join(c.Dir, name)
+
+	if filepath.Dir(path) != c.Dir || filepath.Base(path) != name {
+		return fmt.Errorf("invalid target name %q", name)
+	}
+
+	if err := os.RemoveAll(path); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c Config) ConfigDir(name string) string {
+	return filepath.Join(c.Dir, name)
+}
---
+++ mysql-tools/multisite/config_test.go
@@ -0,0 +1,172 @@
+package multisite_test
+
+import (
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/pivotal-cf/mysql-cli-plugin/mysql-tools/multisite"
+)
+
+var _ = Describe("Config", func() {
+	var subject multisite.Config
+
+	BeforeEach(func() {
+		t, err := os.MkdirTemp("", "multisite_config_test_")
+		Expect(err).NotTo(HaveOccurred())
+		DeferCleanup(func() {
+			Expect(os.RemoveAll(t)).To(Succeed())
+		})
+
+		subject = multisite.Config{Dir: t}
+	})
+
+	Context("ConfigDir", func() {
+		It("provides the path to the .cf home dir for a given target", func() {
+			path := subject.ConfigDir("some-target")
+
+			Expect(path).To(Equal(subject.Dir + "/some-target"))
+
+			otherPath := subject.ConfigDir("some-other-target")
+			Expect(otherPath).To(Equal(subject.Dir + "/some-other-target"))
+		})
+	})
+
+	Context("ListConfigs", func() {
+		It("lists saved multi-site foundation configs", func() {
+			_, _ = subject.SaveConfig("fixtures/sample-config.json", "testFoundation1")
+			_, _ = subject.SaveConfig("fixtures/sample-alt-config.json", "testFoundation2")
+
+			targets, err := subject.ListConfigs()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(targets).To(ConsistOf([]multisite.Target{
+				{
+					Name:         "testFoundation1",
+					Organization: "sample-org",
+					Space:        "sample-space",
+					API:          "https://api.sample.foundation.tld",
+				},
+				{
+					Name:         "testFoundation2",
+					Organization: "alt-sample-org",
+					Space:        "alt-sample-space",
+					API:          "https://api.sample.alt.foundation.tld",
+				},
+			}))
+		})
+
+		When("the cf root directory does not exist", func() {
+			It("does not return an error", func() {
+				_, err := subject.ListConfigs()
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		When("one of the config file paths does not exist", func() {
+			BeforeEach(func() {
+				Expect(os.MkdirAll(subject.Dir+"/testfoundation1/.cf", 0700)).To(Succeed())
+				testConfig := filepath.Join(subject.Dir, "testfoundation1", ".cf", "config.json")
+				Expect(os.Link("fixtures/sample-config.json", testConfig)).To(Succeed())
+
+				Expect(os.MkdirAll(subject.Dir+"/testfoundation2/.cf", 0700)).To(Succeed())
+			})
+
+			It("returns configs that were found", func() {
+				configs, err := subject.ListConfigs()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(configs).To(ContainElement(
+					multisite.Target{
+						Name:         "testfoundation1",
+						Organization: "sample-org",
+						Space:        "sample-space",
+						API:          "https://api.sample.foundation.tld",
+					},
+				))
+			})
+		})
+	})
+
+	Context("SaveConfig", func() {
+		When("saving a valid config", func() {
+			It("returns a JSON subset of relevant config info", func() {
+				savedTarget, err := subject.SaveConfig("fixtures/sample-config.json", "testFoundation")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(savedTarget).NotTo(BeNil())
+				Expect(savedTarget.Organization).To(Equal("sample-org"))
+				Expect(savedTarget.Space).To(Equal("sample-space"))
+				Expect(savedTarget.API).To(Equal("https://api.sample.foundation.tld"))
+			})
+		})
+
+		When("saving two configs to the same name", func() {
+			It("the second save successfully overwrites the first", func() {
+				_, err := subject.SaveConfig("fixtures/sample-config.json", "reusedName")
+				Expect(err).NotTo(HaveOccurred())
+
+				savedTarget, err := subject.SaveConfig("fixtures/sample-alt-config.json", "reusedName")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(savedTarget).NotTo(BeNil())
+
+				Expect(savedTarget.Organization).To(Equal("alt-sample-org"))
+				Expect(savedTarget.Space).To(Equal("alt-sample-space"))
+				Expect(savedTarget.API).To(Equal("https://api.sample.alt.foundation.tld"))
+
+				originalContents, err := os.ReadFile("fixtures/sample-alt-config.json")
+				Expect(err).NotTo(HaveOccurred())
+				savedContents, err := os.ReadFile(filepath.Join(subject.Dir, "reusedName", ".cf", "config.json"))
+				Expect(err).NotTo(HaveOccurred())
+				Expect(string(savedContents)).To(Equal(string(originalContents)), `blah, contents don't match in %q`, subject.Dir)
+			})
+		})
+
+		When("saving a non-existent config file", func() {
+			It("returns a descriptive error", func() {
+				result, err := subject.SaveConfig("/path/does/not/exists", "reusedFoundation")
+				Expect(err).To(MatchError(ContainSubstring("no such file or directory")))
+				Expect(result).To(BeZero())
+			})
+		})
+
+		When("saving a config with corrupted JSON", func() {
+			It("returns a descriptive error", func() {
+
+				result, err := subject.SaveConfig("fixtures/sample-config-with-invalid.json", "reusedFoundation")
+				Expect(err).To(MatchError(ContainSubstring("invalid character")))
+				Expect(result).To(BeZero())
+			})
+		})
+
+		When("saving a config missing required values", func() {
+			It("returns a descriptive error", func() {
+				result, err := subject.SaveConfig("fixtures/sample-config-with-missing-fields.json", "reusedFoundation")
+				Expect(err).To(MatchError(ContainSubstring("saved configuration must target Cloudfoundry: missing fields: [API endpoint,Organization,Space]")))
+				Expect(result).To(BeZero())
+			})
+		})
+	})
+
+	Context("RemoveConfig", func() {
+		It("removes a previously saved configuration", func() {
+			_, err := subject.SaveConfig("fixtures/sample-config.json", "target-name")
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(filepath.Join(subject.Dir, "target-name", ".cf", "config.json")).To(BeAnExistingFile())
+
+			err = subject.RemoveConfig("target-name")
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(filepath.Join(subject.Dir, "target-name", ".cf", "config.json")).ToNot(BeAnExistingFile())
+		})
+
+		It("does not remove the root directory even if a bad name is provided", func() {
+			err := subject.RemoveConfig(".")
+			Expect(err).To(MatchError(`invalid target name "."`))
+			Expect(subject.Dir).To(BeADirectory())
+
+			err = subject.RemoveConfig("../../../etc/shadow")
+			Expect(err).To(MatchError(`invalid target name "../../../etc/shadow"`))
+		})
+	})
+})
