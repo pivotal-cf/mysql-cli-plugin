a1ec092ebc66dd21644b83f458416ab87c7878a5
Andrew Garner <andrew.garner@broadcom.com>
2025-09-05 12:47:00 -0500

    [wip] spring-music updates to address CVES

--- specs/assets/spring-music/src/main/java/org/cloudfoundry/samples/music/repositories/jpa/JpaAlbumRepository.java
+++ specs/assets/spring-music/src/main/java/org/cloudfoundry/samples/music/repositories/jpa/JpaAlbumRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Profile({"in-memory", "mysql"})
 public interface JpaAlbumRepository extends JpaRepository<Album, String> {
 }
